010904123cef4734c53d0511f1e6a3385c631e4a
matheusbourguignon <matheusmbl21@gmail.com>
2024-11-27 19:28:58 -0300

    Finalizando

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,14 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
-        noDiscovery: true
+        noDiscovery: true,
+        include: ['qrcode.vue'] 
     },
     plugins: [
         vue(),
