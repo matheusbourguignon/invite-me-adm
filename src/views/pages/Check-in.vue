9e79204e8395cff9b3b985472a9e824945af50c4
matheusbourguignon <matheusmbl21@gmail.com>
2024-11-27 19:24:29 -0300

    instalando qrcode

--- src/views/pages/Check-in.vue
+++ src/views/pages/Check-in.vue
@@ -1,12 +1,10 @@
 <script setup>
 import FloatingConfigurator from '@/components/FloatingConfigurator.vue';
-import QRCode from 'qrcode.vue'; // Importando o componente QRCode
+import QRCode from 'qrcode.vue';
 import { ref } from 'vue';
 
 const email = ref('');
-const checked = ref(false);
-
-// Função para verificar o check-in
+ 
 const verificarCheckIn = () => {
     if (email.value) {
         alert(`Check-in realizado com sucesso para o email: ${email.value}`);
@@ -27,7 +25,7 @@ const verificarCheckIn = () => {
                     </div>
 
                     <div>
-                        <!-- Campo de email -->
+                
                         <label for="email1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Email</label>
                         <input
                           id="email1"
@@ -41,8 +39,7 @@ const verificarCheckIn = () => {
                         <div v-if="email" class="mb-8 flex justify-center">
                             <QRCode :value="email" size="200" />
                         </div>
-
-                        <!-- Botão de Verificação -->
+                  
                         <button @click="verificarCheckIn" class="btn-check-in w-full p-4 bg-primary-500 text-white rounded-lg">Verificar Check-In</button>   
                     </div>
                 </div>
@@ -50,6 +47,7 @@ const verificarCheckIn = () => {
         </div>
     </div>
 </template>
+
 <style scoped>
 .pi-eye {
     transform: scale(1.6);
