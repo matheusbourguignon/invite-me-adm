7f3c5065c3789d1af504b537091a369416af7090
matheusbourguignon <matheusmbl21@gmail.com>
2024-12-02 14:21:34 -0300

    Rota corrigida

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler,} from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
 
 export const app = fastify()
 
@@ -8,6 +9,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
+app.register(registerForEvent)
 
 app.listen({ port: 3000 }).then(() => {
     console.log('HTTP server running');
