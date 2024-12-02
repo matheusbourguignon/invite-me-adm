886da35c9ff79a4dc281b51740966b520724134f
matheusbourguignon <matheusmbl21@gmail.com>
2024-12-02 10:24:51 -0300

    Adicionando as bibliotecas do prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,22 @@
-interface Pessoa {
-name : string
-age : number    
-}
+import fastify from "fastify";
 
-console.log("Hello word");
+const app = fastify()
+
+app.get('/', () => {
+    return 'Hello NLW Unite'
+})
+
+app.get('/users', () => {
+    return 'Hello Teste'
+})
+
+app.post('/users', () => {
+    return 'Hello Teste'
+})
+
+app.listen({ port: 3000}).then(() => {
+    console.log('HTTP server running');
+    
+})
+
+//localhost:3333
