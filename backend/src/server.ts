32933e8c97d1a9f06892c7b7575fcb9623ebd76c
matheusbourguignon <matheusmbl21@gmail.com>
2024-12-02 13:22:56 -0300

    Adicionando zod provider

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,45 +1,57 @@
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod';
 import { PrismaClient } from '@prisma/client'
-
+import { generateSlug } from "./utils/generate-slug";
 
 const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 const prisma = new PrismaClient({
     log: ['query'],
 })
 
-// Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS,...
+app
+    .withTypeProvider<ZodTypeProvider>()
+    .post('/events', {
+      schema: {
+        body: z.object({
+            title: z.string().min(4),
+            details: z.string().nullable(),
+            maximumAttendees: z.number().int().positive().nullable(),
+        }),
+        response: {
+            201: z.object({
+                eventId: z.string().uuid(),
+            })
+        }
+      },
+    }, async (request, reply) => {
+    const {
+    title,
+    details,
+    maximumAttendees,
+    }  = request.body
 
-// Corpo da requisição (Request Body)
-// Parâmetros de busca (search Params / Query Params)
-// Parâmetros de rota (Route Params) 
-// Cabeçalhos (Headers) 
+    const slug = generateSlug(title)
 
-// Semânticas = Significado
-
-// Driver nativo / Query Builders / Orms
-
-// Object Relational Mapping (Hibernate / Doctrine / ActiveRecord)
-
-// JSON - Javascript Object Notation
-
-app.post('/events', async (request, reply) => {
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-
+    const eventWithSameSlug = await prisma.event.findUnique({
+    where: {
+    slug,
+        }
     })
 
-    const data = createEventSchema.parse(request.body)
-
+    if (eventWithSameSlug !== null) {
+    throw new Error ('Another event with same title already exists.')
+    }
     const event = await prisma.event.create({
         data: {
-            title: data.title,
-            details: data.details,
-            maximumAttendees: data.maximumAttendees,
-            slug: new Date().toISOString(),
+        title,
+        details,
+        maximumAttendees,
+        slug,
         }
     })
 
