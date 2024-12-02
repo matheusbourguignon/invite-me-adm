2abf35064937fd5c8e8b7d15ca157f74b2b2d903
matheusbourguignon <matheusmbl21@gmail.com>
2024-12-02 12:22:35 -0300

    Arrumando a tabela de eventos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,54 @@
 import fastify from "fastify";
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client'
+
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Hello NLW Unite'
+const prisma = new PrismaClient({
+    log: ['query'],
 })
 
-app.get('/users', () => {
-    return 'Hello Teste'
+// Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS,...
+
+// Corpo da requisição (Request Body)
+// Parâmetros de busca (search Params / Query Params)
+// Parâmetros de rota (Route Params) 
+// Cabeçalhos (Headers) 
+
+// Semânticas = Significado
+
+// Driver nativo / Query Builders / Orms
+
+// Object Relational Mapping (Hibernate / Doctrine / ActiveRecord)
+
+// JSON - Javascript Object Notation
+
+app.post('/events', async (request, reply) => {
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        }
+    })
+
+    return { eventId: event.id }
 })
 
-app.post('/users', () => {
-    return 'Hello Teste'
-})
-
-app.listen({ port: 3000}).then(() => {
+app.listen({ port: 3000 }).then(() => {
     console.log('HTTP server running');
-    
+
 })
 
 //localhost:3333
